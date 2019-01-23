b0d5dedfd636da39df1cea6f9ed585391bcead88
Steve Cotter <steve.cotter@kinertia.com>
2019-01-23 12:32:25 -0500

    MaxWidth updates

--- src/MaxWidth/MaxWidth.js
+++ src/MaxWidth/MaxWidth.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default props => (
-  <div style={{ position: 'relative' }}>
-    <div style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: props.maxWidth }}>
-      { props.children }
+export default props => {
+  const styles = {
+    ...(props.styles || {}),
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    maxWidth: props.maxWidth,
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <div style={styles}>
+        { props.children }
+      </div>
     </div>
-  </div>
-)
+  );
+}
