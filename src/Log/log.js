63e05bf2aa89246a7aecd5a4fd011ffef2a223ff
Steve Cotter <steve.cotter@kinertia.com>
2019-02-18 13:01:01 -0500

    Support multiple localhost names in log.

--- src/Log/log.js
+++ src/Log/log.js
@@ -1,4 +1,4 @@
-const debug = window.location.hostname === 'localhost' || window.REACT_DEBUG;
+const debug = window.location.hostname.indexOf('localhost') >= 0 || window.REACT_DEBUG;
 
 export default function() {
   if(debug){
