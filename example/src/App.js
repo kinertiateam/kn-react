808652d90eec89779f5e7eef53b5024781760ab5
chancethe96 <alex@kinertia.com>
2020-07-06 01:15:32 -0400

    fixing duplicate time period

--- example/src/App.js
+++ example/src/App.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Moment from 'moment';
 
-import { stringifyParamsObject, randomString, format, ScrollTo } from 'kn-react';
+import { stringifyParamsObject, randomString, format, ScrollTo, enumerateTimeBetweenMoments } from 'kn-react';
 
 export default class App extends Component {
   render () {
 
-    const paramsObject = {
-      param1: 'abc',
-      param2: 123,
-      param3: 'alex',
-      param4: 'peltier'
-    }
+    // const paramsObject = {
+    //   param1: 'abc',
+    //   param2: 123,
+    //   param3: 'alex',
+    //   param4: 'peltier'
+    // }
 
-    const date = format.dateTime((new Date()),false);
-    const date2 = format.dateTime((new Date()),true);
+    // const date = format.dateTime((new Date()),false);
+    // const date2 = format.dateTime((new Date()),true);
 
-    return (
+    // return (
+    //   <div>
+    //     { randomString() },{ date }, { date2 }
+    //   </div>
+    // )
+
+    const startMoment = Moment('06/02/2020');
+    const endMoment = Moment('07/18/2020');
+
+    // console.log('moments', enumerateTimeBetweenMoments(startMoment, endMoment, 'day', 'day'))
+    console.log('moments', enumerateTimeBetweenMoments(startMoment, endMoment, 'month', 'month'))
+
+    return(
       <div>
-        { randomString() },{ date }, { date2 }
+        Check logs
       </div>
     )
   }
--- src/EnumerateTimeBetweenMoments/enumerateTimeBetweenMoments.js
+++ src/EnumerateTimeBetweenMoments/enumerateTimeBetweenMoments.js
@@ -5,12 +5,15 @@ export default (startMoment, endMoment, startTime='day', addTime='days') => {
   let currentMoment = startMoment.clone().startOf(startTime);
   const lastDate = endMoment.clone().startOf(startTime);
 
-  const days = [startMoment.clone()];
+  const days = [ startMoment.clone() ];
 
-  while(currentMoment.add(1, addTime).diff(lastDate) < 0) {
+  while( currentMoment.add(1, addTime).diff(lastDate) < 0 ) {
     days.push( moment( currentMoment.toDate() ));
   }
 
-  days.push(endMoment.clone())
+  if( days[days.length - 1].diff(lastDate) < 0 ){
+    days.push(endMoment.clone());
+  }
+
   return days;
 };
